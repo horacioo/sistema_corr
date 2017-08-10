47b0c354f0295b173319cd00d3ed5909ddbc03b9
Horácio J <lanterna_@hotmail.com>
2017-08-09 23:42:48 -0300

    start

---
+++ sistema.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: Custom Branding
+Description: Personalizar o formulário de login
+Version: 0.1
+License: GPL
+Author: Vitor Carvalho
+Author URI: http://www.escolawp.com/
+*/
+ 
+// Os snippets de código virão aqui
